05a13f6326f43f16dc9a712392187d12092fb7a5
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-05-16 15:45:33 +0530

    Changed formatting. Changed execution flow of program.

--- hunt.py
+++ hunt.py
@@ -7,87 +7,87 @@ from lxml import html
 from selenium import webdriver
 
 
-
 class UpworkHunt:
-	def __init__(self, patterns, sender, receiver):
-		
-		self.urls = [
-			'https://www.upwork.com/o/jobs/browse/c/web-mobile-software-dev/',
-			'https://www.upwork.com/o/jobs/browse/c/it-networking/',
-			'https://www.upwork.com/o/jobs/browse/c/data-science-analytics/',
-			'https://www.upwork.com/o/jobs/browse/c/engineering-architecture/',
-			'https://www.upwork.com/o/jobs/browse/c/design-creative/',
-			'https://www.upwork.com/o/jobs/browse/c/writing/',
-			'https://www.upwork.com/o/jobs/browse/c/translation/',
-			'https://www.upwork.com/o/jobs/browse/c/legal/',
-			'https://www.upwork.com/o/jobs/browse/c/admin-support/',
-			'https://www.upwork.com/o/jobs/browse/c/customer-service/',
-			'https://www.upwork.com/o/jobs/browse/c/sales-marketing/',
-			'https://www.upwork.com/o/jobs/browse/c/accounting-consulting/'
-		]
-		self.browser = wdstart.start_webdriver('Chrome')
-		self.patterns = patterns
-		self.filtered_jobs = []
-		self.sender_email = sender
-		self.receiver_email = receiver
+    def __init__(self, patterns, sender, receiver):
 
-	def scrape_jobs(self):
-		jobs = []
+        self.urls = [
+            'https://www.upwork.com/o/jobs/browse/c/web-mobile-software-dev/',
+            'https://www.upwork.com/o/jobs/browse/c/it-networking/',
+            'https://www.upwork.com/o/jobs/browse/c/data-science-analytics/',
+            'https://www.upwork.com/o/jobs/browse/c/engineering-architecture/',
+            'https://www.upwork.com/o/jobs/browse/c/design-creative/',
+            'https://www.upwork.com/o/jobs/browse/c/writing/',
+            'https://www.upwork.com/o/jobs/browse/c/translation/',
+            'https://www.upwork.com/o/jobs/browse/c/legal/',
+            'https://www.upwork.com/o/jobs/browse/c/admin-support/',
+            'https://www.upwork.com/o/jobs/browse/c/customer-service/',
+            'https://www.upwork.com/o/jobs/browse/c/sales-marketing/',
+            'https://www.upwork.com/o/jobs/browse/c/accounting-consulting/'
+        ]
+        self.patterns = patterns
+        self.filtered_jobs = []
+        self.sender_email = sender
+        self.receiver_email = receiver
 
-		for url in self.urls:
-			self.browser.get(url)
-			scraped_jobs = self.browser.find_elements_by_class_name('job-tile')[:10]
+    def scrape_jobs(self):
+        jobs = []
 
-			for job in scraped_jobs:
-				job_dict = {
-				'id': job.get_attribute('id')[4:],
-				'link': 'https://www.upwork.com/o/jobs/job/_' + job.get_attribute('id')[4:],
-				'title': job.find_element_by_tag_name('h2').text,
-				'description': job.find_elements_by_tag_name('span')[6].get_attribute('textContent')
-				}
+        for url in self.urls:
+            self.browser = wdstart.start_webdriver('Chrome')
+            self.browser.get(url)
+            scraped_jobs = self.browser.find_elements_by_class_name('job-tile')[:10]
 
-				jobs.append(job_dict)
+            for job in scraped_jobs:
+                job_dict = {
+                    'id': job.get_attribute('id')[4:],
+                    'url': 'https://www.upwork.com/o/jobs/job/_' + job.get_attribute('id')[4:],
+                    'title': job.find_element_by_tag_name('h2').text,
+                    'description': job.find_elements_by_tag_name('span')[6].get_attribute('textContent')
+                }
 
-		return jobs
+                jobs.append(job_dict)
 
-	def filter_jobs(self, jobs):
-		filtered = []
+            self.browser.quit()
 
-		for job in jobs:
-			title = job['title'].lower()
-			desc = job['description'].lower()
+        return jobs
 
-			for pattern in self.patterns:
-				if (pattern.lower() in title) or (pattern.lower() in desc):
-					filtered.append(job)
-					break
+    def filter_jobs(self, jobs):
+        filtered = []
 
-		return filtered
+        for job in jobs:
+            title = job['title'].lower()
+            desc = job['description'].lower()
 
-	def execute(self):
-		scraped_jobs = self.scrape_jobs()
-		jobs = self.filter_jobs(scraped_jobs)
+            for pattern in self.patterns:
+                if (pattern.lower() in title) or (pattern.lower() in desc):
+                    filtered.append(job)
+                    break
 
-		for job in jobs:
-			if not job['id'] in self.filtered_jobs:
-				self.filtered_jobs.append(job['id'])
+        return filtered
 
-				message = 'Title: {}\nDescription: {}\nURL: {}'.format(job['title'], job['description'], job['url'])
-				pygmail.send_email(self.sender_email, self.receiver_email, 'Upwork-Hunt Jobs', message)
+    def execute(self):
+        scraped_jobs = self.scrape_jobs()
+        jobs = self.filter_jobs(scraped_jobs)
 
+        for job in jobs:
+            if not job['id'] in self.filtered_jobs:
+                self.filtered_jobs.append(job['id'])
+
+                message = 'Title: {}\nDescription: {}\nURL: {}'.format(job['title'], job['description'], job['url'])
+                pygmail.send_mail(self.sender_email, self.receiver_email, 'Upwork-Hunt Jobs', message)
 
 
 def main():
-	patterns = input('Enter the pattern strings to search for, seperated by comma: > ').split(',')
-	sender = input('Enter the sender email address [Gmail]: > ')
-	receiver = input('Enter the receiver email address: > ')
+    patterns = input('Enter the pattern strings to search for, seperated by comma: > ').split(',')
+    sender = input('Enter the sender email address [Gmail]: > ')
+    receiver = input('Enter the receiver email address: > ')
 
-	hunter = UpworkHunt(patterns, sender, receiver)
+    hunter = UpworkHunt(patterns, sender, receiver)
 
-	while True:
-		hunter.execute()
-		time.sleep(200)
+    while True:
+        hunter.execute()
+        time.sleep(200)
 
 
-if __name__=='__main__':
-	main()
+if __name__ == '__main__':
+    main()
